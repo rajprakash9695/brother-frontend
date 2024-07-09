6a51e2d967376917eba3dbe13b3cd868da62bc8e
Raj Prakash Maurya <raj.maurya@bksgold.com>
2024-07-09 16:00:07 +0530

    deploy setup

--- build.prod.sh
+++
@@ -1,9 +0,0 @@
-
-
-aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 606635781749.dkr.ecr.ap-south-1.amazonaws.com
-
-docker build -t fe-vault .
-
-docker tag fe-vault:latest 606635781749.dkr.ecr.ap-south-1.amazonaws.com/fe-vault:prod
-
-docker push 606635781749.dkr.ecr.ap-south-1.amazonaws.com/fe-vault:prod
